3d7521e03eee103d8b2bc57d6690f30cdb53ee7c
udev <bees9633@gmail.com>
2023-06-01 17:46:23 -0700

    Update Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// attempt at trying to call C++ methods from C# as shown in this tutorial: https://stackoverflow.com/questions/9407616/how-to-write-my-c-function-so-i-can-call-it-from-c
 
 // Also see: https://learn.microsoft.com/en-us/cpp/dotnet/calling-native-functions-from-managed-code?view=msvc-1704
 // Another useful resource: https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.dllimportattribute?view=net-7.0
-using System;
+//using System;
 using System.Runtime.InteropServices;
 
 const string dllPath = "lib/libwindow_test.dll";
